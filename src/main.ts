2488a1403c412b7a686e1985ef6e844b08a5c302
Quentin Gérome <q.gerome@whatever-company.com>
2020-02-12 11:13:03 +0100

    chore: Add the environment url

--- src/main.ts
+++ src/main.ts
@@ -16,19 +16,24 @@ async function run() {
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
     const token = core.getInput("token", { required: true });
-    const url = core.getInput("target_url", { required: false }) || defaultUrl;
+    const url = core.getInput("log_url", { required: false }) || defaultUrl;
+    const environmentUrl = core.getInput("environment_url", {
+      required: false
+    });
     const description = core.getInput("description", { required: false }) || "";
     const deploymentId = core.getInput("deployment_id");
     const state = core.getInput("state") as DeploymentState;
-
+    const autoInactive =
+      core.getInput("auto_inactive", { required: false }) === "true";
     const client = new github.GitHub(token);
 
     await client.repos.createDeploymentStatus({
       ...context.repo,
       deployment_id: parseInt(deploymentId),
       state,
-      log_url: defaultUrl,
-      target_url: url,
+      log_url: url,
+      environment_url: environmentUrl,
+      auto_inactive: autoInactive,
       description
     });
   } catch (error) {
